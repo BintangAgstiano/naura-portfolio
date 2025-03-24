7924833e6168e8a77ac37e77ae112f4f0eee7c59
BintangAgstiano <bintangagustiano3@gmail.com>
2025-03-24 14:19:01 +0700

    update

--- pl.js
+++ pl.js
@@ -5,3 +5,16 @@
 //     menuIcon.classList.toggle('bx-x')
 //     navbar.classList.toggle('active');
 // }
+
+let iconMenu = document.querySelector('#menu-icon');
+let linkNav = document.querySelector('.link-nav-hp');
+let isClik = false;
+iconMenu.addEventListener('click', function () {
+    if (isClik) {
+        linkNav.style.left = '0%'
+        isClik = false
+    } else {
+        linkNav.style.left = '-80%'
+        isClik = true
+    }
+})
